cfaec03f37f8d8183c2cb400be835c7277f59838
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-12 15:39:40 -0300

    feat: define 'Megaphone' class

--- cpp-module-00/ex00/megaphone.cpp
+++ cpp-module-00/ex00/megaphone.cpp
@@ -1,3 +1,29 @@
-int main() {
+// Copyright (c) 2023 hmigl <hmigl@student.42sp.org.br>. All rights reserved.
+
+#include <iostream>
+
+class Megaphone {
+ private:
+  int argc_;
+  char **argv_;
+
+  Megaphone() {}
+  Megaphone(int argc, char **argv) : argc_(argc), argv_(argv) {}
+
+ public:
+  static Megaphone FromArgs(int argc, char *argv[]) {
+    return Megaphone(argc, argv);
+  }
+
+  void MakeNoise() const {
+    if (this->argc_ == 1)
+      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  Megaphone megaphone = Megaphone::FromArgs(argc, argv);
+
+  megaphone.MakeNoise();
   return 0;
 }
