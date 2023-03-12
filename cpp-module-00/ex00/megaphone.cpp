6f98d18eba3466119fa3b68b741dac75f02d7b7d
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-12 15:59:37 -0300

    feat: output argv as uppercase strings

--- cpp-module-00/ex00/megaphone.cpp
+++ cpp-module-00/ex00/megaphone.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2023 hmigl <hmigl@student.42sp.org.br>. All rights reserved.
 
+#include <cstring>
 #include <iostream>
 
 class Megaphone {
@@ -16,8 +17,21 @@ class Megaphone {
   }
 
   void MakeNoise() const {
-    if (this->argc_ == 1)
-      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    int len = 0;
+
+    if (this->argc_ == 1) {
+      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    } else {
+      for (int i = 1; i < this->argc_; i++) {
+        len = strlen(this->argv_[i]);
+
+        for (int j = 0; j < len; j++) {
+          char c = toupper(this->argv_[i][j]);
+          std::cout << c;
+        }
+      }
+      std::cout << '\n';
+    }
   }
 };
 
