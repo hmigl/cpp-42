40e373fe157d1ca60977fa003b49ddf05aa29e00
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-13 10:47:06 -0300

    feat(cpp00): create main() to run program

---
+++ cpp-module-00/ex01/main.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 hmigl <hmigl@student.42sp.org.br>. All rights reserved.
+
+#include "./PhoneBook.hpp"
+
+int main(void) {
+  PhoneBook phonebook;
+
+  phonebook.run();
+  return 0;
+}
