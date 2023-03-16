5b78f5b73ce66e8db8b78fb3940ab318d82db747
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-16 20:26:23 -0300

    feat: create initial files

---
+++ cpp-module-01/ex01/Zombie.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 hmigl <hmigl@student.42sp.org.br>. All rights reserved.
+
+#include <iostream>
+#include <string>
+
+class Zombie {
+ private:
+  std::string name;
+
+ public:
+  explicit Zombie(const std::string &name);
+  ~Zombie();
+
+  void announce(void) const;
+};
