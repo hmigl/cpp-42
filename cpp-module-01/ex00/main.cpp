2612b8acbc8a690a29b4298b3121f63521035c7f
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-16 17:37:07 -0300

    feat: implement 'randomChump()'

--- cpp-module-01/ex00/main.cpp
+++ cpp-module-01/ex00/main.cpp
@@ -3,13 +3,16 @@
 #include "./Zombie.hpp"
 
 Zombie *newZombie(std::string name);
+void randomChump(std::string name);
 
 int main(void) {
   Zombie z("Foo");                // stack allocation
   Zombie *zz = newZombie("Bar");  // heap allocation
+  randomChump("FooBar");          // stack allocation
 
   z.announce();
   zz->announce();
   delete zz;  // deallocate the memory
+
   return 0;
 }
