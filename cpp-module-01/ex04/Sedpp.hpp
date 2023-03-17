20c31dc4b629bb647bddcf14923b9c406013922f
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-17 15:31:46 -0300

    feat: define Sedpp.hpp

---
+++ cpp-module-01/ex04/Sedpp.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 hmigl <hmigl@student.42sp.org.br>. All rights reserved.
+
+#ifndef SEDPP_HPP_
+#define SEDPP_HPP_
+
+#include <string>
+
+class Sedpp {
+ private:
+  std::string filename_;
+  std::string contents_;
+
+  explicit Sedpp(const std::string &filename);
+
+ public:
+  ~Sedpp();
+
+  static Sedpp fromFilename(const std::string &filename);
+  void replace(const std::string &s1, const std::string &s2);
+};
+
+#endif  // SEDPP_HPP_
