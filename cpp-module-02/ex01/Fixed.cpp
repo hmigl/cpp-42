a9aa729d0d91230628dd01cc181494169d659be7
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-21 16:33:47 -0300

    refactor: copy and modify Fixed.cpp, with required methods/constructors

---
+++ cpp-module-02/ex01/Fixed.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 13:03:04 by hmigl             #+#    #+#             */
+/*   Updated: 2023/03/21 08:24:23 by hmigl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./Fixed.hpp"
+
+#include <iostream>
+
+Fixed::Fixed() : rawBits_(0) { std::cout << "Default constructor called\n"; }
+
+Fixed::Fixed(const Fixed &other) {
+  std::cout << "Copy constructor called\n";
+  *this = other;
+}
+
+Fixed::Fixed(const int value) { std::cout << "Int constructor called\n"; }
+
+Fixed::Fixed(const float value) { std::cout << "Float constructor called\n"; }
+
+Fixed::~Fixed() { std::cout << "Destructor called\n"; }
+
+float Fixed::toFloat() const { return 0; }
+
+int Fixed::toInt() const { return 0; }
+
+int Fixed::getRawBits() const {
+  std::cout << "getRawBits member function called\n";
+  return rawBits_;
+}
+
+void Fixed::setRawBits(const int rawBits) { rawBits_ = rawBits; }
+
+Fixed &Fixed::operator=(const Fixed &other) {
+  std::cout << "Copy assignment operator called\n";
+  if (this != &other) {
+    rawBits_ = other.getRawBits();
+  }
+  return *this;
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &obj) { return os; }
