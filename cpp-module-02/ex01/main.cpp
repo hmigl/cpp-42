a84d962dfbae8f3b91db9d3d767ea2c0a35a006f
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-21 16:54:09 -0300

    feat: add main()

---
+++ cpp-module-02/ex01/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/19 13:39:44 by hmigl             #+#    #+#             */
+/*   Updated: 2023/03/21 16:47:54 by hmigl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "./Fixed.hpp"
+
+int main(void) {
+  Fixed a;
+  Fixed const b(10);
+  Fixed const c(42.42f);
+  Fixed const d(b);
+
+  a = Fixed(1234.4321f);
+
+  std::cout << "a is " << a << std::endl;
+  std::cout << "b is " << b << std::endl;
+  std::cout << "c is " << c << std::endl;
+  std::cout << "d is " << d << std::endl;
+
+  std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+  std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+  std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+  std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+  return 0;
+}
