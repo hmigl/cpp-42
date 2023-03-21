26b848cf718f7a2d49f1a1f613c367820ede7a5f
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-21 19:36:09 -0300

    refactor: remove 'debug' messages for better readability

--- cpp-module-02/ex02/Fixed.cpp
+++ cpp-module-02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 13:03:04 by hmigl             #+#    #+#             */
-/*   Updated: 2023/03/21 19:18:18 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/03/21 19:33:34 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,15 @@
 #include <cmath>
 #include <iostream>
 
-Fixed::Fixed() : rawBits_(0) { std::cout << "Default constructor called\n"; }
+Fixed::Fixed() : rawBits_(0) {}
 
-Fixed::Fixed(const Fixed &other) {
-  std::cout << "Copy constructor called\n";
-  *this = other;
-}
+Fixed::Fixed(const Fixed &other) { *this = other; }
 
-Fixed::Fixed(const int num) : rawBits_(num << FractionalBits) {
-  std::cout << "Int constructor called\n";
-}
+Fixed::Fixed(const int num) : rawBits_(num << FractionalBits) {}
 
-Fixed::Fixed(const float num) : rawBits_(roundf(num * (1 << FractionalBits))) {
-  std::cout << "Float constructor called\n";
-}
+Fixed::Fixed(const float num) : rawBits_(roundf(num * (1 << FractionalBits))) {}
 
-Fixed::~Fixed() { std::cout << "Destructor called\n"; }
+Fixed::~Fixed() {}
 
 float Fixed::toFloat() const {
   return static_cast<float>(rawBits_) / (1 << FractionalBits);
@@ -38,15 +31,11 @@ float Fixed::toFloat() const {
 
 int Fixed::toInt() const { return rawBits_ >> FractionalBits; }
 
-int Fixed::getRawBits() const {
-  std::cout << "getRawBits member function called\n";
-  return rawBits_;
-}
+int Fixed::getRawBits() const { return rawBits_; }
 
 void Fixed::setRawBits(const int rawBits) { rawBits_ = rawBits; }
 
 Fixed &Fixed::operator=(const Fixed &other) {
-  std::cout << "Copy assignment operator called\n";
   if (this != &other) {
     rawBits_ = other.getRawBits();
   }
