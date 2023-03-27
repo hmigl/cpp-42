1ae4c4c20808fb2e94e6ccf1e2ff961e4e759fa8
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-27 18:05:40 -0300

    test: add tests for Cat

--- cpp-module-04/ex00/main.cpp
+++ cpp-module-04/ex00/main.cpp
@@ -6,28 +6,38 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 17:51:39 by hmigl             #+#    #+#             */
-/*   Updated: 2023/03/27 17:51:40 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/03/27 18:00:01 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cassert>
 
+#include "./Cat.hpp"
 #include "./Dog.hpp"
 
 int main(void) {
-  {
-    const Animal *dog = new Dog();
-
-    assert(!dog->getType().compare("Dog"));
-    std::cout << dog->getType() << '\n';
-    delete dog;
-  }
   {
     const Animal *animal = new Animal();
 
     assert(!animal->getType().compare("Undefined"));
     std::cout << animal->getType() << '\n';
     delete animal;
+    std::cout << '\n';
+  }
+  {
+    const Animal *dog = new Dog();
+
+    assert(!dog->getType().compare("Dog"));
+    std::cout << dog->getType() << '\n';
+    delete dog;
+    std::cout << '\n';
+  }
+  {
+    const Animal *cat = new Cat();
+
+    assert(!cat->getType().compare("Cat"));
+    std::cout << cat->getType() << '\n';
+    delete cat;
   }
   return 0;
 }
