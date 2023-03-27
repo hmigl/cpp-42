ad517c344240158cb9f3fb57ff4f4783c1ae59b0
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-27 07:09:03 -0300

    refactor: remove unnecessary tests

--- cpp-module-03/ex03/main.cpp
+++ cpp-module-03/ex03/main.cpp
@@ -6,77 +6,26 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 10:36:24 by hmigl             #+#    #+#             */
-/*   Updated: 2023/03/24 12:36:25 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/03/25 11:06:50 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cassert>
 #include <iostream>
 
-#include "./ClapTrap.hpp"
-#include "./FragTrap.hpp"
-#include "./ScavTrap.hpp"
+#include "./DiamondTrap.hpp"
 
 int main(void) {
-  {
-    std::cout << "[ClapTrap]\n";
-    ClapTrap porfiry("Porfiry Petrovitch");
-    assert(porfiry.getHitPoints() == 10);
-    assert(porfiry.getEnergyPoints() == 10);
-    assert(porfiry.getAttackDamage() == 0);
+  std::cout << "[DiamondTrap]\n";
+  DiamondTrap nikolai("Nikolai Vsevolodovich Stavrogin");
+  assert(nikolai.getHitPoints() == 100);    // FragTrap
+  assert(nikolai.getEnergyPoints() == 50);  // ScavTrap
+  assert(nikolai.getAttackDamage() == 30);  // FragTrap
 
-    porfiry.attack("Rodion Raskolnikov");
-    porfiry.takeDamage(5);
-    porfiry.takeDamage(1866);
-    porfiry.takeDamage(1872);
-    porfiry.attack("Rodion Raskolnikov");
-    porfiry.beRepaired(1880);
-  }
-  {
-    std::cout << "\n[ScavTrap]\n";
-    ScavTrap raskolnikov("Rodion Raskolnikov");
-    assert(raskolnikov.getHitPoints() == 100);
-    assert(raskolnikov.getEnergyPoints() == 50);
-    assert(raskolnikov.getAttackDamage() == 20);
+  nikolai.attack("Lizaveta Nikolaevna Tushina");  // ScavTrap
+  nikolai.whoAmI();                               // DiamondTrap
+  nikolai.highFiveGuys();                         // FragTrap
+  nikolai.guardGate();                            // ScavTrap
 
-    raskolnikov.attack("Porfiry Petrovitch");
-    raskolnikov.takeDamage(15);
-    raskolnikov.takeDamage(1866);
-    raskolnikov.takeDamage(1872);
-    raskolnikov.attack("Porfiry Petrovitch");
-    raskolnikov.beRepaired(1880);
-    raskolnikov.guardGate();
-  }
-  {
-    std::cout << "\n[ScavTrap]\n";
-    ScavTrap parfyon("Parfyon Semyonovich Rogozhin");
-    ScavTrap rogozhin(parfyon);
-
-    rogozhin.attack("Myshkin");
-    rogozhin.takeDamage(5);
-    rogozhin.takeDamage(1866);
-    assert(rogozhin.getHitPoints() == 0);
-
-    rogozhin.takeDamage(1872);
-    rogozhin.attack("Nastasya Filippovna");
-    rogozhin.beRepaired(1880);
-    assert(rogozhin.getHitPoints() == 1880);
-  }
-  {
-    std::cout << "\n[FragTrap]\n";
-    FragTrap nastasya("Nastasya Filippovna");
-    assert(nastasya.getHitPoints() == 100);
-    assert(nastasya.getEnergyPoints() == 100);
-    assert(nastasya.getAttackDamage() == 30);
-
-    nastasya.attack("Gavril Ardalyonovich Ivolgin");
-    nastasya.takeDamage(15);
-    nastasya.takeDamage(1866);
-    nastasya.takeDamage(1872);
-    nastasya.attack("Porfiry Petrovitch");
-    nastasya.highFiveGuys();
-    nastasya.beRepaired(1880);
-    nastasya.highFiveGuys();
-  }
   return 0;
 }
