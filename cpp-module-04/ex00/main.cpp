bc4c7d8a3e3694c091683778c0a5b868bdc98899
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-27 17:56:00 -0300

    test: add tests for Animal and Dog classes

--- cpp-module-04/ex00/main.cpp
+++ cpp-module-04/ex00/main.cpp
@@ -1,1 +1,33 @@
-int main(void) { return 0; }
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 17:51:39 by hmigl             #+#    #+#             */
+/*   Updated: 2023/03/27 17:51:40 by hmigl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cassert>
+
+#include "./Dog.hpp"
+
+int main(void) {
+  {
+    const Animal *dog = new Dog();
+
+    assert(!dog->getType().compare("Dog"));
+    std::cout << dog->getType() << '\n';
+    delete dog;
+  }
+  {
+    const Animal *animal = new Animal();
+
+    assert(!animal->getType().compare("Undefined"));
+    std::cout << animal->getType() << '\n';
+    delete animal;
+  }
+  return 0;
+}
