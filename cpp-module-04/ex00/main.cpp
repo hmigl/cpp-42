cb4c0623794fb87de92ab9a942f68aac0292da69
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-28 07:56:05 -0300

    test: add tests for WrongAnimal and WrongCat classes

--- cpp-module-04/ex00/main.cpp
+++ cpp-module-04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 17:51:39 by hmigl             #+#    #+#             */
-/*   Updated: 2023/03/27 18:06:27 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/03/27 18:53:54 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include "./Cat.hpp"
 #include "./Dog.hpp"
+#include "./WrongCat.hpp"
 
 int main(void) {
   {
@@ -41,6 +42,24 @@ int main(void) {
     std::cout << cat->getType() << '\n';
     cat->makeSound();
     delete cat;
+    std::cout << '\n';
+  }
+  {
+    const WrongAnimal *wrongAnimal = new WrongAnimal();
+
+    assert(!wrongAnimal->getType().compare("Undefined"));
+    std::cout << wrongAnimal->getType() << '\n';
+    wrongAnimal->makeSound();
+    delete wrongAnimal;
+    std::cout << '\n';
+  }
+  {
+    const WrongAnimal *wrongCat = new WrongCat();
+
+    assert(!wrongCat->getType().compare("Wrong Cat"));
+    std::cout << wrongCat->getType() << '\n';  // early binding, no polymorphism
+    wrongCat->makeSound();
+    delete wrongCat;
   }
   return 0;
 }
