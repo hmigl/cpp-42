7e3ee98f5bb2a6d865ef9f61fc2e9544b17564bd
hmigl <88746406+hmigl@users.noreply.github.com>
2023-03-28 17:10:20 -0300

    test: check if deep copy is used

--- cpp-module-04/ex01/main.cpp
+++ cpp-module-04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 17:51:39 by hmigl             #+#    #+#             */
-/*   Updated: 2023/03/28 16:37:29 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/03/28 17:07:55 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,42 @@
 #include "./WrongCat.hpp"
 
 const int NbAnimals = 4;
+
+static void deepCopyTest() {
+  std::cout << "____________________________________\n";
+  std::cout << "DEEP COPY CHECK:\n\n";
+  {
+    Dog *dog = new Dog();
+    Dog *dcDog = new Dog(*dog);
+
+    std::cout << "dog: " << dog->getBrain() << std::endl;
+    std::cout << "deep copy dog: " << dcDog->getBrain() << std::endl;
+    delete dog;
+    delete dcDog;
+  }
+  std::cout << "\n____________________________________\n";
+  {
+    Cat *cat = new Cat();
+    Cat *dcCat = new Cat(*cat);
+
+    std::cout << "cat: " << cat->getBrain() << std::endl;
+    std::cout << "deep copy cat: " << dcCat->getBrain() << std::endl;
+    delete cat;
+    delete dcCat;
+  }
+  std::cout << "\n____________________________________\n";
+  {
+    Dog *dog = new Dog();
+    {
+      Dog *dcDog = new Dog(*dog);
+      std::cout << "deep copy dog: " << dcDog->getBrain() << std::endl;
+      delete dcDog;
+    }
+    std::cout << "dog: " << dog->getBrain()
+              << std::endl;  // if it was a shallow copy, it would have
+                             // undefined behavior, since 'dcDog' is gone
+  }
+}
 
 int main(void) {
   const Animal *animals[NbAnimals];
@@ -31,12 +67,13 @@ int main(void) {
     animals[i]->makeSound();
     std::cout << std::endl;
   }
-  std::cout << "------------------------------------\n";
+  std::cout << "____________________________________\n";
 
   std::cout << "DELETING ARRAY OF ANIMAL OBJECTS:\n\n";
   for (int i = 0; i < NbAnimals; ++i) {
     delete animals[i];
     std::cout << std::endl;
   }
+  deepCopyTest();
   return 0;
 }
