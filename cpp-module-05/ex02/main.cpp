17ce6383d89e08784256990c93192fe7201bce45
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-02 20:45:49 -0300

    test: add test for RobotomyRequestForm

--- cpp-module-05/ex02/main.cpp
+++ cpp-module-05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 19:50:56 by hmigl             #+#    #+#             */
-/*   Updated: 2023/04/02 20:22:09 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/04/02 20:36:42 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,24 @@
 #include "./PresidentialPardonForm.hpp"
 #include "./RobotomyRequestForm.hpp"
 #include "./ShrubberyCreationForm.hpp"
+
+void shouldValidateRobotomyRequestForm() {
+  std::cout << "shouldValidateRobotomyRequestForm\n";
+
+  RobotomyRequestForm form("Jarvis");
+  std::cout << form << std::endl;
+  assert(form.getName() == "RobotomyRequestForm");
+  assert(form.getRequiredGradeToSign() == 72);
+  assert(form.getRequiredGradeToExecute() == 45);
+  try {
+    Bureaucrat b("Beauro Crat", 15);
+    std::cout << b << std::endl;
+    form.beSigned(b);
+    form.execute(b);
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+}
 
 void shouldValidatePresidentialPardonForm() {
   std::cout << "shouldValidatePresidentialPardonForm\n";
@@ -29,12 +47,14 @@ void shouldValidatePresidentialPardonForm() {
     std::cout << b << std::endl;
     form.beSigned(b);
     form.execute(b);
-  } catch (const AForm::GradeTooLowException &e) {
+  } catch (const std::exception &e) {
     std::cout << e.what() << std::endl;
   }
+  std::cout << std::endl;
 }
 
 int main() {
   shouldValidatePresidentialPardonForm();
+  shouldValidateRobotomyRequestForm();
   return 0;
 }
