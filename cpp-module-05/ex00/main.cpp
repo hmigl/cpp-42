1bd276d35952b5a86047f4a2edc0975cafde34ff
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-02 09:39:19 -0300

    test: add tests for 'decrementGrade()'

--- cpp-module-05/ex00/main.cpp
+++ cpp-module-05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 18:13:52 by hmigl             #+#    #+#             */
-/*   Updated: 2023/04/02 09:33:57 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/04/02 09:37:39 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,18 @@
 #include "./Bureaucrat.hpp"
 
 const std::string &Name = "Beauro Crat";
+
+void shouldDecrementGrade() {
+  std::cout << "shouldDecrementGrade\n";
+
+  std::cout << "creating bureaucrat with grade == 42\n";
+  Bureaucrat b(Name, 42);
+  for (int i = 0; i < 10; ++i) {
+    b.decrementGrade();
+  }
+  std::cout << "asserting that grade == 52\n";
+  assert(b.getGrade() == 52);
+}
 
 void shouldIncrementGrade() {
   std::cout << "shouldIncrementGrade\n";
@@ -27,6 +39,7 @@ void shouldIncrementGrade() {
   }
   std::cout << "asserting that grade == 140\n";
   assert(b.getGrade() == 140);
+  std::cout << std::endl;
 }
 
 void shouldThrowGradeTooHighException() {
@@ -76,5 +89,6 @@ int main() {
   shouldThrowGradeTooLowException();
   shouldThrowGradeTooHighException();
   shouldIncrementGrade();
+  shouldDecrementGrade();
   return 0;
 }
