c2f7ce88b093a349e791a33eb8244dc8e75f37ee
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-11 08:39:30 -0300

    refactor: remove header from main.cpp

--- cpp-module-06/ex00/main.cpp
+++ cpp-module-06/ex00/main.cpp
@@ -6,11 +6,9 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 12:12:05 by hmigl             #+#    #+#             */
-/*   Updated: 2023/04/10 14:32:30 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/04/11 08:39:20 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <iostream>
 
 #include "./ScalarConverter.hpp"
 
