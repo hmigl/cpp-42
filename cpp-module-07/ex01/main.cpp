b69e3dd5299a76e28e03d254117711f03d5c9748
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-13 08:39:20 -0300

    test: create tests to produce exceptions due to nullptr or 0 length

--- cpp-module-07/ex01/main.cpp
+++ cpp-module-07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 08:36:57 by hmigl             #+#    #+#             */
-/*   Updated: 2023/04/13 08:37:02 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/04/13 08:45:59 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,27 @@ int main() {
     int arr[] = {1, 2, 3, 4, 5};
     iter(arr, 5, &print<int>);
     std::cout << std::endl;
+  }
+  std::cout << std::endl;
+  {
+    std::cout << "should throw invalid_argument exception due to nullptr:\n";
+    try {
+      int *arr = NULL;
+      iter(arr, 5, &print<int>);
+    } catch (const std::invalid_argument &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+  std::cout << std::endl;
+  {
+    std::cout << "should throw invalid_argument exception due to zero passed "
+                 "as length:\n";
+    try {
+      int arr[] = {1, 2, 3, 4, 5};
+      iter(arr, 0, &print<int>);
+    } catch (const std::invalid_argument &e) {
+      std::cout << e.what() << std::endl;
+    }
   }
   return 0;
 }
