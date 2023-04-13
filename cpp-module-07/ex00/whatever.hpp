618ebcee6c0ee295af9014f15943a8064f1bb7d8
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-13 07:48:16 -0300

    feat: define 'max()' function template

--- cpp-module-07/ex00/whatever.hpp
+++ cpp-module-07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 07:33:49 by hmigl             #+#    #+#             */
-/*   Updated: 2023/04/13 07:37:31 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/04/13 07:46:30 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,9 @@ void swap(T &lhs, T &rhs) {
 template <typename T>
 T min(T lhs, T rhs) {
   return lhs < rhs ? lhs : rhs;
+}
+
+template <typename T>
+T max(T lhs, T rhs) {
+  return lhs > rhs ? lhs : rhs;
 }
