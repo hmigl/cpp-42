51f9afcc6e8d52c2e0a01b27f7c424e52baa8b27
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-14 08:04:57 -0300

    test: create test to multiply int[] elements

--- cpp-module-07/ex01/main.cpp
+++ cpp-module-07/ex01/main.cpp
@@ -6,13 +6,16 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 08:36:57 by hmigl             #+#    #+#             */
-/*   Updated: 2023/04/13 08:45:59 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/04/14 08:04:42 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cctype>
 #include <iostream>
 
 #include "./iter.hpp"
+
+void multiply(int &n) { n *= 10; }
 
 template <typename T>
 void print(const T &val) {
@@ -46,6 +49,14 @@ int main() {
     } catch (const std::invalid_argument &e) {
       std::cout << e.what() << std::endl;
     }
+  }
+  std::cout << std::endl;
+  {
+    std::cout << "should print all elements multiplied by 10:\n";
+    int arr[] = {1, 2, 3, 4, 5};
+    iter<int>(arr, 5, &multiply);
+    iter<int>(arr, 5, &print);
+    std::cout << std::endl;
   }
   return 0;
 }
