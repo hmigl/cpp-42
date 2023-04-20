504b460bf6164f025665b597deb46f2193b185c9
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-20 13:53:29 -0300

    test: include elements without exceeding capacity

--- cpp-module-08/ex01/main.cpp
+++ cpp-module-08/ex01/main.cpp
@@ -6,13 +6,31 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 11:11:09 by hmigl             #+#    #+#             */
-/*   Updated: 2023/04/20 13:16:47 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/04/20 13:20:36 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 #include "./Span.hpp"
+
+void shouldNotAddSequenceThatExceedsCapacity() {
+  std::vector<int> v;
+  for (int i = 0; i < 10000; ++i) {
+    v.push_back(i);
+  }
+  std::vector<int>::iterator first = v.begin();
+  std::vector<int>::iterator last = v.end();
+
+  Span s(10000);
+  s.addNumber(-1);
+  try {
+    s.insertRange(first, last);
+  } catch (const std::length_error &e) {
+    // std::cout << e.what() << std::endl;
+  }
+  std::cout << "should not add sequence that exceeds capacity [OK]\n";
+}
 
 void shouldAddSequenceOfElements() {
   std::vector<int> v;
@@ -36,6 +54,7 @@ void shouldNotAddNumber() {
   try {
     s.addNumber(42);
   } catch (const std::length_error &e) {
+    // std::cout << e.what() << std::endl;
   }
   std::cout << "should not add number [OK]\n";
 }
@@ -43,5 +62,6 @@ void shouldNotAddNumber() {
 int main() {
   shouldNotAddNumber();
   shouldAddSequenceOfElements();
+  shouldNotAddSequenceThatExceedsCapacity();
   return 0;
 }
