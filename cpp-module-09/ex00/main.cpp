6c2482768f5a1e26f6b6d807d557c26cfc9b5f28
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-27 11:09:26 -0300

    feat: add 'main()'

--- cpp-module-09/ex00/main.cpp
+++ cpp-module-09/ex00/main.cpp
@@ -1,3 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 11:09:22 by hmigl             #+#    #+#             */
+/*   Updated: 2023/04/27 11:09:22 by hmigl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
 #include "./BitcoinExchange.hpp"
 
-int main() { return 0; }
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "usage: \'./btc <amount_history_file>\'\n";
+    return 1;
+  }
+
+  try {
+    BitcoinExchange be = BitcoinExchange::from_amount_history_file(argv[1]);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
+}
