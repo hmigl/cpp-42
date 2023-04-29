f7fb315b149cc9d182aae56acf4e0c8e32780ad0
hmigl <88746406+hmigl@users.noreply.github.com>
2023-04-29 10:16:45 -0300

    wip: start parsing integer sequence

--- cpp-module-09/ex02/PmergeMe.cpp
+++ cpp-module-09/ex02/PmergeMe.cpp
@@ -1,8 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 10:13:24 by hmigl             #+#    #+#             */
+/*   Updated: 2023/04/29 10:15:28 by hmigl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./PmergeMe.hpp"
 
 PmergeMe::PmergeMe() {}
 
-PmergeMe::PmergeMe(const char **) {}
+PmergeMe::PmergeMe(const char **seq) {
+  if (seq == NULL || (*seq) == NULL) {
+    throw std::invalid_argument("yadayada");
+  }
+  for (size_t i = 0; seq[i]; ++i) {
+    const char *str = seq[i];
+    while (*str) {
+      if (!std::isdigit(*str)) {
+        throw std::invalid_argument("yadayada2");
+      }
+      ++str;
+    }
+    int n = std::atol(seq[i]);
+    if (n < 1 || n > std::numeric_limits<int>::max()) {
+      throw std::invalid_argument("yadayada3");
+    }
+  }
+  std::cout << "42069\n";
+}
 
 PmergeMe::PmergeMe(const PmergeMe &) {}
 
--- cpp-module-09/ex02/PmergeMe.hpp
+++ cpp-module-09/ex02/PmergeMe.hpp
@@ -6,13 +6,17 @@
 /*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 09:22:45 by hmigl             #+#    #+#             */
-/*   Updated: 2023/04/29 09:24:37 by hmigl            ###   ########.fr       */
+/*   Updated: 2023/04/29 09:59:10 by hmigl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <limits>
+#include <stdexcept>
 
 class PmergeMe {
  public:
